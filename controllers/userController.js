f1f314f5baf0609c1a675a0466191e5c9d96d3e1
Parinya Khemthong <earth084@hotmail.co.th>
2022-08-08 12:53:21 +0700

    not delete

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,46 +1,91 @@
-const User = require('../models/userModel');
-const bcrypt = require('bcrypt');
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
-exports.getAllUser = async (req, res, next) =>{
-    const user = await User.find().then((result) =>{
-        res.status(200).json(result);
-    }).catch((error) =>{
-        res.status(422).json({
-            message:"get all users error",
-            error:error
-        })
+exports.getAllUser = async (req, res, next) => {
+  const user = await User.find()
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((error) => {
+      res.status(422).json({
+        message: "get all users error",
+        error: error,
+      });
     });
 };
 
-exports.updateUser = async (req, res, next) =>{
-    // userId used in userRoute
-    const userId = req.params.userId;
-    await User.findById(userId).then((result) =>{
-        console.log(result);
-        if(!result) {
-            return res.status(400).json({
-                message:"wrong user"
-            })
-        };
+exports.updateUser = async (req, res, next) => {
+  // userId used in userRoute
+  try {
+    const saltRound = await bcrypt.genSalt(12);
+    const _id = req.params.userId;
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(400).json({
+        message: "wrong user",
+      });
+    }
+    //
+    req.body.password = await bcrypt.hash(req.body.password, saltRound);
+    const updateUser = await User.findByIdAndUpdate(_id, req.body, {
+      new: true,
+    });
+    res.status(200).json({
+      message: "Profile updated",
+      updateUser,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "update error",
+      error: error,
+    });
+  }
 
-        // verify password
-        const saltRound = await bcrypt.genSalt(12);
-        req.body.password = await bcrypt.hash(req.body.password, saltRound);
-        const updateUser = await User.findByIdAndUpdate(userId, req.body,{
-            new: true,
-       });
-
-       res.status(200).json({
-        message:"profile updated",
-        updateUser,
-       });
-
-
-    }).catch((error)=>{
-        console.log("update user error, not match");
-        res.status(400).json({
-            message:"update error",
-            error:error
-        });
-    })
+  //   const { name, password, username, email, profile } = req.body;
+  //   const userId = req.params.userId;
+  //   const hashedPassword = await bcrypt.hash(password, saltRound);
+  //   const updateInfo = {
+  //     name: name,
+  //     username: username,
+  //     email: email,
+  //     password: password,
+  //     profile: profile,
+  //   };
+  //   await User.findByIdAndUpdate(userId, updateInfo)
+  //     .select("name password username email")
+  //     .then((result) => {
+  //       console.log(`OLD:${password} : NEW ${password}`);
+  //       console.log("profile updated", result);
+  //       res.status(200).json({
+  //         message: "profile updated",
+  //         result,
+  //       });
+  //     })
+  //     .catch((error) => {
+  //       res.status(400).json({
+  //         message: "user id invalid!",
+  //       });
+  //     });
 };
+
+// exports.deleteUser = async (req, res, next) =>{
+//     const _id = req.params.userId;
+//     const user = await User.findById(_id);
+//     if (!user) {
+//       return res.status(400).json({
+//         message: "wrong user",
+//       });
+//     };
+//     const deletedUser = await User.findByIdAndDelete(_id).then((result) =>{
+//         res.status(200).json({
+//             message:"delete user success",
+//             deletedUser
+//         });
+//     }).catch((error) =>{
+//         console.log(error)
+//         res.status(400).json({
+//             message:"delete user error",
+//             error:error
+//         });
+//     })
+// };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,9 @@
 const userRoute = require('express').Router();
-const { getAllUser, updateUser } = require('../controllers/userController');
+const { getAllUser, updateUser, deleteUser } = require('../controllers/userController');
 const { authMiddleware } = require('../middleware/auth');
 
 userRoute.get("/all", authMiddleware, getAllUser);
 userRoute.put("/:userId", authMiddleware, updateUser);
+// userRoute.delete("/:userId", authMiddleware, deleteUser);
 
 module.exports = userRoute;
